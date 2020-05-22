de6e15f1785f4ed9d8bb97ece6ad7e9bdb467b0a
REIS0 <aor@cesar.school>
2020-05-22 17:56:33 -0300

    Update instalation

--- install_spark_docker.sh
+++ install_spark_docker.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 
-# Rodar o script dentro de uma pasta
+# Rodar o script dentro de uma pasta!!
 
 # pega o nome da pasta
 localdir=$(pwd)
 
+# Cria a imagem
+docker build -t "spark:Dockerfile"
+
 # roda o container e instala a dependencia graphframe
 # -v vai sincronizar a pasta 'work' dentro do container com a pasta local
-docker run --name spark -v $localdir:/home/jovyan/work -p8888:8888 -d jupyter/pyspark-notebook && docker exec -it spark pip install graphframes
+docker run --name spark -v $localdir:/home/jovya
+n/work -p8888:8888 -d spark:Dockerfile
 
 # mostra os logs que contem o token de acesso
 docker logs spark
